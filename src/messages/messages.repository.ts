02e8db409ccee293a999a9d97887732235e9acc7
Jim124 <fengpingan065@gmail.com>
2024-07-03 17:15:56 +0800

    use DI container

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,25 +1,27 @@
-import { readFile,writeFile } from "fs/promises";
+import { Injectable } from "@nestjs/common";
+import { readFile, writeFile } from "fs/promises";
 
-export class MessagesRepository{
-  async findOne(id: string){
-    const contents = await readFile('messages.json','utf8');
+@Injectable()
+export class MessagesRepository {
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
     return messages[id];
   }
-  async findAll(){
-    const contents = await readFile('messages.json','utf8');
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
     return messages;
- 
+
   }
-  async create(content:string){
-    const contents = await readFile('messages.json','utf8');
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
     const id = Math.floor(Math.random() * 999);
-    
-    messages[id] = {id,content}
 
-    await writeFile('messages.json',JSON.stringify(messages));
+    messages[id] = { id, content }
+
+    await writeFile('messages.json', JSON.stringify(messages));
 
   }
 }
